f7d0b067fff1370f04a9a653eea6fdcac51746a4
j-preim <j.preimesberger@gmail.com>
2024-03-06 11:38:24 -0600

    framework

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import useVerifyUser from '../hooks/useVerifyUser'
+
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+export default function Home() {
+
+  
+
+  return (
+    <>
+      
+    
+    </>
+  )
+}
+

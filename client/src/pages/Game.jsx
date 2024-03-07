9810b4bab7a781ee596535caa151a698152c2c8c
j-preim <j.preimesberger@gmail.com>
2024-03-07 11:09:36 -0600

    committing maze game files

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 import useVerifyUser from '../hooks/useVerifyUser'
 
-import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 
-export default function Home() {
+export default function Game() {
 
-  
 
   return (
     <>
       <h1>The Legend of Dizzy</h1>
+
+      <div id="maze_container"></div>
     
     </>
   )

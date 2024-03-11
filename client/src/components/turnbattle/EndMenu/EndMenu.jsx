d99407e08157550fee2124bea5c97241b9a411d6
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-03-11 18:24:49 -0500

    chaining games

--- client/src/components/turnbattle/EndMenu/EndMenu.jsx
+++ client/src/components/turnbattle/EndMenu/EndMenu.jsx
@@ -1,10 +1,20 @@
 import styles from './styles.module.css';
+import { useDispatch } from 'react-redux';
 
-export const EndMenu = ({ winner, onStartClick }) => {
+
+export const EndMenu = ({ winner }) => {
+  const dispatch = useDispatch();
+
+  const changeStage1 = () => {
+    dispatch({ type: 'SET_STAGENAME', payload: "stage1" });
+
+  };
+
   return (
     <div className={styles.main}>
       <h1>{winner.name} has won!</h1>
-      <button className={styles.startButton} onClick={onStartClick}>
+      <button className={styles.startButton} onClick={changeStage1}
+      >
         Next Game
       </button>
     </div>
--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -3,7 +3,7 @@ import useVerifyUser from "../hooks/useVerifyUser";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 // const GAME_URL = "../../snes-rpg/levels/open-field.html";
-// import { AppTurnBattle} from "../components/turnbattle";
+import { AppTurnBattle} from "../components/turnbattle";
 // import { OpenField } from "../../snes-rpg/levels/OpenField";
 // import Canvas from "../components/Canvas";
 import TargetPractice from '../components/targetpractice/TargetPractice/TargetPractice'
@@ -18,43 +18,43 @@ import { useSelector } from 'react-redux';
 export default function Game() {
 
   const [gameUrl, setGameUrl] = useState("../../snes-rpg/levels/open-field.html");
-  const [stageName, setStageName] = useState("start");
   const [currentScore, setCurrentScore] = useState(0)
 
-  const scoreNow = useSelector((state) => state.shooterScore);
+  const stageName = useSelector((state) => state.stagename);
+
+  
 
 
   return (
     <>
-    <h5>{stageName} {scoreNow}</h5>
+    <Header />
       <div>
-      {stageName === "start" && (
-          <GameOver setStageName={setStageName} onStartClick={() => setStageName("game1") } />
-        )}
-        
-        {/* {stageName === "start" && (
-          <Canvas gameUrl={gameUrl} setGameUrl={setGameUrl} onStartClick={() => setStageName("game2") } />
+      {/* {stageName === "start" && (
+          <TargetPractice setStageName={setStageName} onStartClick={() => setStageName("game1") } />
         )} */}
         
-        {/* {stageName === "game1" && (
-          <Matterjs onStartClick={() => setStageName("game2") } />
+        {stageName === "start" && (
+          <AppTurnBattle />
+        )}
+        
+        {stageName === "stage1" && (
+          <GameOver />
         )}
 
-        {stageName === "game2" && (
+        {/* {stageName === "game2" && (
           <AppTurnBattle setCurrentScore={setCurrentScore} onStartClick={() => setStageName("game3") } />
-        )}
-
-        {stageName === "game3" && (
-          <Canvas gameUrl={gameUrl} setGameUrl={setGameUrl} stageName={stageName} setStageName={setStageName} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
-          )}
-
-        {stageName === "gameOver" && (
-          <GameOver onStartClick={() => setStageName("game3") } />
         )} */}
+
+        {/* {stageName === "game3" && (
+          <Canvas gameUrl={gameUrl} setGameUrl={setGameUrl} stageName={stageName} setStageName={setStageName} currentScore={currentScore} setCurrentScore={setCurrentScore}/>
+          )} */}
+
+        {/* {stageName === "gameOver" && (
+          <GameOver onStartClick={() => setStageName("game3") } />
+        )} } */}
 
 
       </div>
-    <TargetPractice />
     </>
   );
 }

f39724c2a29eacbdfb662cb3d7f2a4a2a83a6150
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-03-10 23:55:59 -0500

    going to bed

---
+++ client/src/components/Leaderboard.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import '../output.css';
+import '../assets/css/header.css';
+
+const Leaderboard = () => {
+  
+
+  return (
+    <>
+
+    {/* maybe you just update the game over function to do the leaderboard logic and leave this to simply get all on highscores?
+    
+    we can legit handle it all here....but we could also just have one db and the highscore db is part of the exersize so maybe do it there?*/}
+      
+    </>
+  );
+};
+
+export default Leaderboard;

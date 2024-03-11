4e7a86191a5c6caced97356b864a6e037385a527
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-03-11 12:23:56 -0500

    prepping for pull

--- client/src/components/GameOver.jsx
+++ client/src/components/GameOver.jsx
@@ -7,3 +7,22 @@ export const GameOver = ({ onStartClick }) => {
       </div>
     );
   };
+
+  /*
+What this needs to do top to bottom:
+
+- Display finishing message
+
+- Add all three scores from state 
+and push that number into the scores array in User
+
+- Get all users
+
+- Parse through all user scores, grab the top five
+and post to HighScores
+
+- Provide button that links to a contact page
+
+- Provide a button to go Home
+
+  */
--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,19 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import '../output.css';
-import '../assets/css/header.css';
 
 const Leaderboard = () => {
-  
+  // const [leaderboardData, setLeaderboardData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchLeaderboard = async () => {
+  //     try {
+  //       const response = await fetch("/api/user");
+  //       const data = await response.json();
+
+  //       const sortedData = data.payload.sort((a, b) => b.scores[0] - a.scores[0]);
+  //       const top5 = sortedData.slice(0, 5);
+
+  //       await postHighScores(top5);
+  //     } catch (error) {
+  //       console.error("Error fetching leaderboard:", error);
+  //     }
+  //   };
+
+  //   const postHighScores = async (top5) => {
+  //     try {
+  //       const highScores = top5.map(({ name, email, scores }) => ({
+  //         name,
+  //         email,
+  //         score: scores[0],
+  //       }));
+
+  //       const response = await fetch("/api/highscores", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(highScores),
+  //       });
+
+  //       if (response.ok) {
+  //         console.log("High scores posted successfully!");
+  //       } else {
+  //         console.error("Failed to post high scores:", response.statusText);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error posting high scores:", error);
+  //     }
+  //   };
+
+  //   fetchLeaderboard();
+  // }, []); // The empty dependency array ensures that the effect runs only once when the component mounts
 
   return (
-    <>
-
-    {/* maybe you just update the game over function to do the leaderboard logic and leave this to simply get all on highscores?
-    
-    we can legit handle it all here....but we could also just have one db and the highscore db is part of the exersize so maybe do it there?*/}
-      
-    </>
+    <div>
+      {/* Render your leaderboard UI using the 'leaderboardData' state */}
+    </div>
   );
 };
 

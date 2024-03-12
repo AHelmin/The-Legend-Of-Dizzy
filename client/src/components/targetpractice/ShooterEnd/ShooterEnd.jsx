cf64a2102d95665e321c15e8033260f4e967143e
helm0127 <helm0127@morris.umn.edu>
2024-03-11 22:50:33 -0500

    added styling to start,end buttons, added line to zero angularvelocity in arrow game

--- client/src/components/targetpractice/ShooterEnd/ShooterEnd.jsx
+++ client/src/components/targetpractice/ShooterEnd/ShooterEnd.jsx
@@ -4,16 +4,19 @@ import { useDispatch } from 'react-redux';
 export default function ShooterEnd({ score }) {
     const dispatch = useDispatch()
     const changeStage2 = () => {
-        dispatch({ type: 'SET_STAGENAME', payload: 'stage2'})
+        dispatch({ type: 'SET_STAGENAME', payload: 'stage2' })
     }
     return (
         //   <div className={styles.main}>
-        <div>
+        <>
             <h1 className={styles.h1}>Congratulations! You scored {score} points!
                 You hit the target {score / 5} times!</h1>
-            <button className={styles.nextButton} onClick={changeStage2}>
-                Next Game
-            </button>
-        </div>
+            <div className={styles.main}>
+
+                <button className="bg-green-800 hover:bg-green-700 text-yellow-400 font-bold py-2 px-4 rounded hyrule" onClick={changeStage2}>
+                    Next Game
+                </button>
+            </div>
+        </>
     );
 };

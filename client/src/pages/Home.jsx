6851cb551c069bf1fc887a06708c054ee1fd6baa
j-preim <j.preimesberger@gmail.com>
2024-03-12 22:19:28 -0500

    updated homepage formatting

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,16 +13,16 @@ const Home = () => {
     <div>
       <Header />
 
-      <div className="text-center mt-8">
+      <div className="text-center mt-8 flex flex-col h-full">
         <img
           src={zeldadog}
           alt="Centered Image"
-          className="mx-auto h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 homepage-image"
+          className="mx-auto h-auto max-w-lg transition-all duration-300 rounded-lg filter grayscale hover:grayscale-0 homepage-image"
         />
 
         <div className="mt-4">
           <h1 className="text-4xl font-bold hyrule text-yellow-600">Welcome to The Legend of Dizzy</h1>
-          <p className="text-lg mt-2 press-start text-white">
+          <p className="text-lg mt-8 press-start text-white">
           {useTypedMessage('Explore a world of adventure and challenges in this legendary game.')}
           </p>
         </div>
